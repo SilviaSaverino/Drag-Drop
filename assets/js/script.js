c9b6f9d8e7872bef75b0f395dadb6c97116eda33
Silvia Saverino <silviasave89@gmail.com>
2023-07-28 16:04:27 +0100

    implement initial drang and drop functionalities

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const fill = document.querySelectorAll('.fill')
+const box = document.querySelectorAll('.box')
+
+function dragStart() {
+    console.log('drag start')
+}
+
+function dragEnd() {
+    console.log('drag end')
+}
+
+function dragHover() {
+    console.log('drag hover')
+}
+
+function dragDrop() {
+    console.log('drag drop')
+}
+
+function dragEnter() {
+    console.log('drag enter')
+}
+
+function dragLeave() {
+    console.log('drag leave')
+}
