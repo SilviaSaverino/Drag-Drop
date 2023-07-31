ac247acbaecc65435065d0a83bd9b78242376194
Silvia Saverino <silviasave89@gmail.com>
2023-07-31 13:01:13 +0100

    convert nodeList into an array with images, add draggedIndex and amend drop function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,53 @@
-const fill = document.querySelectorAll('.fill')
-const boxes = document.querySelectorAll('.box')
+const fill = document.querySelectorAll('.fill');
+const boxes = document.querySelectorAll('.box');
+const images = Array.from(fill);
 
-fill.forEach(fill => {
-    fill.addEventListener('dragstart', dragStart);
-});
-fill.forEach(fill => {
+fill.forEach((fill, index) => {
+    fill.addEventListener('dragstart', (e) => dragStart(e, index));
     fill.addEventListener('dragend', dragEnd);
 });
 
-for(const box of boxes){
-    box.addEventListener('dragover', dragOver),
-    box.addEventListener('dragenter', dragEnter),
-    box.addEventListener('dragleave', dragLeave),
-    box.addEventListener('drop', Drop)
+for (const box of boxes) {
+    box.addEventListener('dragover', dragOver);
+    box.addEventListener('dragenter', dragEnter);
+    box.addEventListener('dragleave', dragLeave);
+    box.addEventListener('drop', Drop);
 }
 
-function dragStart() {
-    console.log('drag start')
-    this.className += ' hold'
-    setTimeout(()=> this.className = 'invisible', 0)
+function dragStart(e, index) {
+    console.log('drag start');
+    draggedIndex = index;
+    this.classList.add('hold');
+    setTimeout(() => this.classList.add('invisible'), 0);
 }
-    
 
 function dragEnd() {
-    console.log('drag end')
-    this.className = 'fill'
+    console.log('drag end');
+    this.classList.remove('invisible');
+    this.classList.remove('hold');
 }
 
 function dragOver(e) {
-    console.log('drag over')
-    e.preventDefault()
+    console.log('drag over');
+    e.preventDefault();
 }
 
 function dragEnter(e) {
-    console.log('drag enter')
-    e.preventDefault()
+    console.log('drag enter');
+    e.preventDefault();
+    this.classList.add('hovered');
 }
 
 function dragLeave() {
-    console.log('drag leave')
+    console.log('drag leave');
+    this.classList.remove('hovered');
 }
 
 function Drop() {
-    console.log('drag drop')
-    this.className = 'box'
-    this.append(fill)
+    console.log('drag drop');
+    if (draggedIndex !== null) {
+        const draggedImage = images[draggedIndex];
+        this.append(draggedImage);
+        draggedIndex = null;
+    }
 }
